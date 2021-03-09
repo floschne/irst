995e294221125762a8a2df1a35a49a0fc035d162
floschne <florian.schneider.1992@gmx.de>
2021-03-09 22:04:54 +0100

    fixed logger plugin

--- app/plugins/logger.js
+++ app/plugins/logger.js
@@ -2,14 +2,29 @@
 export const logger = (lvl, msg) => {
   const m = JSON.stringify(msg)
   if (lvl === 'error' || lvl === 'err' || lvl === 'e')
-    if (window !== undefined) window.console.error(m)
-    else console.error(m)
+    try {
+      window.console.error(m)
+    } catch (e) {
+      // window not defined
+      // eslint-disable-next-line no-console
+      console.error(m)
+    }
   else if (lvl === 'warning' || lvl === 'warn' || lvl === 'w')
-    if (window !== undefined) window.console.warn(m)
-    else console.warn(m)
+    try {
+      window.console.warn(m)
+    } catch (e) {
+      // window not defined
+      // eslint-disable-next-line no-console
+      console.warn(m)
+    }
   else if (lvl === 'info' || lvl === 'i')
-    if (window !== undefined) window.console.info(m)
-    else console.info(m)
+    try {
+      window.console.info(m)
+    } catch (e) {
+      // window not defined
+      // eslint-disable-next-line no-console
+      console.info(m)
+    }
 }
 
 export default ({ app }, inject) => {
