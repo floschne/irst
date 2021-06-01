fb121fb7062e9f0c3df5f3bc1346d660dc6bb859
floschne <florian.schneider.1992@gmx.de>
2021-06-01 20:05:50 +0200

    added user id to BaseResults

--- api/models/base_result.py
+++ api/models/base_result.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, validator, root_validator
 from shortuuid import uuid
 
 import models
@@ -13,7 +13,21 @@ class BaseResult(BaseModel):
     id: str = Field(description="BaseResult UUID", default_factory=uuid)
     sample_id: str = Field(description="UUID of the related BaseSample")
     created: datetime = Field(description="Timestamp of the BaseResult", default_factory=datetime.now)
-    mt_params: Optional[MTurkParams] = Field(description="Optional MTurk Parameters", default=None)
+    mt_params: Optional[MTurkParams] = Field(
+        description="Optional MTurk Parameters. Either MTurk Parameters or User ID has to be not None", default=None)
+    user_id: Optional[str] = Field(
+        description="Optional User ID. Either MTurk Parameters or User ID has to be not None", default=None)
+
+    @root_validator
+    def user_id_or_mturk_not_none(cls, values):
+        if models.__validation_disabled__:
+            return values
+
+        mt_params = values.get('mt_params')
+        user_id = values.get('user_id')
+        if mt_params is None and user_id is None:
+            raise ValueError(f"Either MTurk Parameters or User ID has to be not None!")
+        return values
 
     @validator('sample_id')
     def sample_must_exist(cls, sample_id: str):
